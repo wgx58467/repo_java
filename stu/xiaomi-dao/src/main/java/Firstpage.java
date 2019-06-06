c916ffbf2a2421b8c3494229084bf13028001643
wgx <wgx@163.com>
2019-06-06 20:21:27 +0800

    啊啊

---
+++ stu/xiaomi-dao/src/main/java/Firstpage.java
@@ -0,0 +1,5 @@
+public class Firstpage {
+    public String show() {
+        return "Hello World! The new day!";
+    }
+}
---
+++ stu/xiaomi-service/src/main/java/Second.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class Second {
+    public static void main(String[] args) {
+        Firstpage aa = new Firstpage();
+        System.out.println(aa.show());
+    }
+}
