eee2257aa689009c7282e49412c9d13ad86b43ed
wgx <wgx@163.com>
2019-06-06 22:58:32 +0800

    FirstClass02

--- stu_mis/src/com/hist/git/FirstClass.java
+++ stu_mis/src/com/hist/git/FirstClass.java
@@ -5,5 +5,6 @@ public class FirstClass {
         System.out.println("Hello World.");
         System.out.println("Hello World.");
         System.out.println("Hello World.");
+        System.out.println("Hello World.");
     }
 }
